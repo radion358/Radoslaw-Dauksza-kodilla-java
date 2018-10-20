cdc442784a9f5bebb7d9652bd9e5a11e14bd616a
radion358 <radoslaw.dauksza@gmail.com>
2018-10-21 00:19:02 +0200

    task 7.1 (practicing lambda expression)

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,22 +1,17 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.beautifier.PoemBeautifier;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        PoemBeautifier beautifier = new PoemBeautifier();
 
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        beautifier.beautify("Lubię placki.", poem -> "ABC " + poem + " ABC" );
+        System.out.println();
+        beautifier.beautify("Lubię placki.", poem -> poem.toUpperCase());
+        System.out.println();
+        beautifier.beautify("Lubię placki.", poem -> poem.replaceFirst(" ", "\n"));
+        System.out.println();
+        beautifier.beautify("Lubię placki.", poem -> poem.replaceFirst("placki", "ser"));
     }
 }
