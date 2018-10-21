2599e5eab278d9ac31f967fb7fc5ebf637a125ad
radion358 <radoslaw.dauksza@gmail.com>
2018-10-21 22:20:18 +0200

    Learning about filter(), map() & collect()

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,21 +1,18 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier beautifier = new PoemBeautifier();
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
 
-        beautifier.beautify("Lubię placki.", poem -> "ABC " + poem + " ABC" );
-        System.out.println();
-        beautifier.beautify("Lubię placki.", poem -> poem.toUpperCase());
-        System.out.println();
-        beautifier.beautify("Lubię placki.", poem -> poem.replaceFirst(" ", "\n"));
-        System.out.println();
-        beautifier.beautify("Lubię placki.", poem -> poem.replaceFirst("placki", "ser"));
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        System.out.println(theResultStringOfBooks);
     }
 }
