ef64958217f112921d1af6d5c4983b23b8512e97
radion358 <radoslaw.dauksza@gmail.com>
2018-10-21 11:09:07 +0200

    Learning about Stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -13,5 +14,8 @@ public class StreamMain {
         beautifier.beautify("Lubię placki.", poem -> poem.replaceFirst(" ", "\n"));
         System.out.println();
         beautifier.beautify("Lubię placki.", poem -> poem.replaceFirst("placki", "ser"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
