4489bc72143d5f6e0b57d73114990e95bc1ece99
radion358 <radoslaw.dauksza@gmail.com>
2018-10-26 20:59:12 +0200

    Task 8.2

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,33 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+
+        try {
+            if (b == 0) {
+                throw new ArithmeticException();
+            }
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("You are trying to divide by 0!" + e);
+            return -1;
+        }finally {
+            System.out.println("If you was trying to divide by 0 you get result -1");
+        }
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        double result = firstChallenge.divide(3, 0);
+
+        System.out.println(result);
+
+    }
+}
